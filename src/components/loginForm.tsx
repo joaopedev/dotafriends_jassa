39d32883f70cca18c68f1a8991931ece7390708f
joaopedev <joaopedev@outlook.com>
2024-01-05 08:12:51 -0300

    Finish input for send email.

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
   Button,
   FormControl,
@@ -10,67 +10,61 @@ import {
   Text,
   ChakraProvider,
 } from "@chakra-ui/react";
-import { useAuth } from "../context/authContext";
-import { useNavigate } from "react-router-dom";
 import png from "../images/dota-2-logo-A8CAC9B4C9-seeklogo.com.png";
 import themeCinzel from "../utils/fontCinzel";
+import axios from "axios";
+
 
 export const LoginForm: React.FC = () => {
-  const { login, isAuthenticated } = useAuth();
   const [email, setEmail] = useState("");
-  const navigate = useNavigate();
-  const [emailError, setEmailError] = useState(false);
-  console.log(emailError);
+  const handleSendClick = async () => {
+    try {
+      const response = await axios.post(`http://localhost:3005/enviar-email`, {
+        email: email
+      });
 
-  const handleLogin = () => {
-    if (isAuthenticated) {
-      navigate("/home", { state: { email } });
-    } else {
-      if (email) {
-        login(email);
-        setEmailError(false);
-      } else {
-        setEmailError(true);
-      }
+      console.log("Resposta da API:", response.data);
+    } catch (error) {
+      console.error("Erro ao fazer a requisição:", error);
     }
   };
 
-  useEffect(() => {
-    if (isAuthenticated) {
-      navigate("/home", { state: { email } });
-    }
-  }, [isAuthenticated, navigate, email]);
-
   return (
     <ChakraProvider theme={themeCinzel}>
-    <Box boxShadow="md" rounded="md" bg="White" maxW="100%" p={6}>
-      <VStack spacing={3}>
-        <Image m={3} maxW="30%" src={png} />
-        <FormControl m={3}>
-          <VStack>
-            <FormLabel color="black" m={3}>
-              Welcome to Jassa DOTA 2 friends!
-            </FormLabel>
-            <Text m={3} color="black"> If you want a good friend to play DOTA 2 with, enter your email to receive an email and let's get started!</Text>
-          </VStack>
+      <Box boxShadow="md" rounded="md" bg="White" maxW="100%" p={6}>
+        <VStack spacing={3}>
+          <Image m={3} maxW="30%" src={png} />
+          <FormControl m={3}>
+            <VStack>
+              <FormLabel color="black" m={3}>
+                Welcome to Jassa DOTA 2 friends!
+              </FormLabel>
+              <Text m={3} color="black">
+                {" "}
+                If you want a good friend to play DOTA 2 with, enter your email
+                to receive an email and let's get started!
+              </Text>
+            </VStack>
 
-          <FormLabel color="black" m={3}>Email:</FormLabel>
-          <VStack>
-          <Input
-          borderColor="black"
-            m={3}
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          </VStack>
-        </FormControl>
-        <Button mb={5} colorScheme="red" onClick={handleLogin}>
-          Send
-        </Button>
-      </VStack>
-    </Box>
+            <FormLabel color="black" m={3}>
+              Email:
+            </FormLabel>
+            <VStack>
+              <Input
+              textColor="black"
+                borderColor="black"
+                m={3}
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </VStack>
+          </FormControl>
+          <Button mb={5} colorScheme="red" onClick={handleSendClick}>
+            Send
+          </Button>
+        </VStack>
+      </Box>
     </ChakraProvider>
   );
 };
